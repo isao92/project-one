e8d567c664ae337de52b53e9f8701395f971d93e
Taylor Walker <taylor.walker@hotmail.com>
2018-11-30 10:32:27 -0800

    html started

---
+++ assets/javascript/ap.js
@@ -0,0 +1,31 @@
+function searchLastFM(artist) {
+
+    var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist + "&api_key=1dcfb244939573ecb6439c31080d44c0&format=json";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var artBio = $("<p>").text(response.artist.bio.summary);
+        var artImg = $("<img>").attr("src", response.artist.image[2]["#text"])
+        artImg.attr("class", "art-pic");
+        artBio.attr("class", "art-bio");
+
+        // if(!response.artist.url) {
+        //     alert("no url found");
+       
+        $("#img-bio").empty();
+        $("#img-bio").append(artImg, artBio);      
+    });
+}
+
+$("#select-artist").on("click", function (event) {   
+    event.preventDefault();
+    var inputArtist = $("#artist-input").val().trim();
+    searchLastFM(inputArtist);
+    // searchLastFM(inputArtist);
+});
+
+
+
+
